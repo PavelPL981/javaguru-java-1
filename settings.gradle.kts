96e8442692b5cf06d17412ad0d0e91ab5f6effde
Pavel_WPC <p.pigas@uccholding.com>
2026-08-17 14:19:07 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "javaguru-java-1"
---
+++ src/main/java/lesson_1/HelloWorld.java
@@ -0,0 +1,8 @@
+package lesson_1;
+
+public class HelloWorld {
+
+    static void main() {
+        System.out.println("Hello World");
+    }
+}
