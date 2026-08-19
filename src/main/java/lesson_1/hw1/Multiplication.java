818ca8b49a0ead31a43d262aa9b9b0bb4d2e11fb
Pavel_WPC <p.pigas@uccholding.com>
2026-08-19 08:46:52 +0500

    Lesson 1_Homework 1

---
+++ src/main/java/lesson_1/hw1/Multiplication.java
@@ -0,0 +1,13 @@
+package lesson_1.hw1;
+
+public class Multiplication {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static void main() {
+        int multiply = multiply(50, 3);
+        System.out.println(multiply);
+    }
+}
---
+++ src/main/java/lesson_1/hw1/Summa.java
@@ -0,0 +1,11 @@
+package lesson_1.hw1;
+
+public class Summa {
+
+    static void main() {
+        int a = 74;
+        int b = 36;
+        int result = a + b;
+        System.out.println(result);
+    }
+}
